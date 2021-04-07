ccbff242a6c64c09c08857752ea7a0d5b273c042
Aaqib Hafeez <aaqib7@hotmail.co.uk>
2021-04-07 01:17:47 +0100

    .NET Core 3.1 Serverless project created with a customer controller.

---
+++ SwaggerAPILambda/src/SwaggerAPILambda/Controllers/CustomerController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SwaggerAPILambda.Controllers
+{
+    [Route("api/[controller]")]
+    public class CustomerController : Controller
+    {
+
+        [HttpGet]
+        public ActionResult<Customer> Get()
+        {
+            return new OkObjectResult(new Customer()
+            {
+                Name = "John",
+                Age = 30
+            });
+        }
+
+        [HttpPost]
+        public ActionResult Create()
+        {
+            return new OkObjectResult("Customer Created");
+        }
+    }
+
+    public class Customer
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
