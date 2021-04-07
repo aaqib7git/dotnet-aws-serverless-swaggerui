301b62c1e4ee520d24343cc5fae2344e8c1f2e32
Aaqib Hafeez <aaqib7@hotmail.co.uk>
2021-04-07 03:51:20 +0100

    cleanup code

--- SwaggerAPILambda/src/SwaggerAPILambda/Controllers/CustomerController.cs
+++ SwaggerAPILambda/src/SwaggerAPILambda/Controllers/CustomerController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
